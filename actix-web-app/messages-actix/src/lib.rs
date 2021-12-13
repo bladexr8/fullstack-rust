0bbef7a0a1221ffcacad983ba5592323b474f447
Stephen Moss <bladexr8@gmail.com>
2021-12-13 22:16:00 +1100

    inital app state

--- actix-web-app/messages-actix/src/lib.rs
+++ actix-web-app/messages-actix/src/lib.rs
@@ -1,13 +1,33 @@
 #[macro_use]
 extern crate actix_web;
 
+// web server modules from Actix
 use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
+// JSON Serialise/De-serialise
 use serde::Serialize;
+
+// facilitate mutating properties with an immutable struct
+use std::cell::Cell;
+
+// tools to work with usize atomically and thread-safe
+use std::sync::atomic::{AtomicUsize, Ordering};
+// tools to safely share and mutate non-atomic things across multiple threads
+use std::sync::{Arc, Mutex};
+
+// track count of workers
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 // publicly accessible struct
 // Note port is private
 pub struct MessageApp {
     port: u16,
+}
+
+// application state (each worker gets own instance)
+struct AppState {
+    server_id: usize,
+    request_count: Cell<usize>,
+    messages: Arc<Mutex<Vec<String>>>,
 }
 
 // implementation of MessageApp struct
@@ -20,9 +40,17 @@ impl MessageApp {
 
     // boot up web server
     pub fn run(&self) -> std::io::Result<()> {
+        // share messages across workers
+        let messages = Arc::new(Mutex::new(vec![]));
         println!("Starting http server: 127.0.0.1:{}", self.port);
         HttpServer::new(move || {
             App::new()
+                // add application state
+                .data(AppState {
+                    server_id: SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
+                    request_count: Cell::new(0),
+                    messages: messages.clone(),
+                })
                 .wrap(middleware::Logger::default())
                 .service(index)
         })
@@ -35,18 +63,24 @@ impl MessageApp {
 // Request Handler
 #[derive(Serialize)]
 struct IndexResponse {
-    message: String,
+    server_id: usize,
+    request_count: usize,
+    messages: Vec<String>,
 }
 
 #[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
-    let hello = req
-        .headers()
-        .get("hello")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+
+    // update # requests for this server
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    // access data inside the Mutex
+    let ms = state.messages.lock().unwrap();
+    
 
     Ok(web::Json(IndexResponse {
-        message: hello.to_owned(),
+        server_id: state.server_id,
+        request_count,
+        messages: ms.clone(),
     }))
 }
